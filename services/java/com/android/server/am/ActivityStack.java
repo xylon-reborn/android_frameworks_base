a22491e858041b1e92c130be8d82e685866d3873
Mårten Kongstad <marten.kongstad@sonyericsson.com>
2011-12-05 13:10:36 +0100

    Make ActivityManagerService.systemReady thread safe
    During boot of the framework, two threads execute code in
    ActivityManagerService and ActivityStack, respectively. At one point
    in time, the latter thread checks the internal state of the first
    thread, but if for some reason that thread has stalled for an
    unexpected amount of time, the state has not been updated (yet).
    
    An example of the above is when one of the services started from
    SystemServer (eg AppWidgetService) takes unexpectedly long to start.
    This causes the framework to boot without broadcasting
    BOOT_COMPLETED, which in turn has ramifications for the entire
    system.
    
    Change-Id: I14b5f6fcb320be01596a2e04db3f724a43f4fbe1

--- services/java/com/android/server/am/ActivityStack.java
+++ services/java/com/android/server/am/ActivityStack.java
@@ -3668,6 +3668,10 @@ final class ActivityStack {
                 mService.mCancelledThumbnails.clear();
             }
 
+        }
+
+        mService.mBootingCondition.block();
+        synchronized (mService) {
             if (mMainStack) {
                 booting = mService.mBooting;
                 mService.mBooting = false;
