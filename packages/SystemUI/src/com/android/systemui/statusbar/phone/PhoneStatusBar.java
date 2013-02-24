a5ab48f6712923e162cbd3955718ca74177a49a5
Lars Greiss <kufikugel@googlemail.com>
2013-02-24 11:32:33 +0100

    Frameworks: Fix possible NPE
    cheers
    
    Change-Id: I9c694a749689b0019df4f1d82f06dc56ee79efc2

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1899,7 +1899,10 @@ public class PhoneStatusBar extends BaseStatusBar {
     }
 
     public boolean isShowingSettings() {
-        return mHasFlipSettings && mFlipSettingsView.getVisibility() == View.VISIBLE;
+        if (mFlipSettingsView != null) {
+            return mHasFlipSettings && mFlipSettingsView.getVisibility() == View.VISIBLE;
+        }
+        return false;
     }
 
     public void completePartialFlip() {
