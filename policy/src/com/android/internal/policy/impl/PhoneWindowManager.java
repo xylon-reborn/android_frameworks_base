c81cb364cde5f1f7c4cdc4c749f42b32892fe9e7
liquid0624 <rjbirnesser@gmail.com>
2013-02-26 20:17:46 -0500

    Remove Kill App Longpress Back/Adjust Timeout
    This is replaced by hardware key bindings.
    Under interface settings you can bind the
    back key to perform the same functionality.
    Adjusted back press timeout value to 1000ms

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -2595,12 +2595,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             return -1;
         } else if (keyCode == KeyEvent.KEYCODE_BACK) {
             if (down) {
-                if (Settings.Secure.getInt(mContext.getContentResolver(),
-                        Settings.Secure.KILL_APP_LONGPRESS_BACK, 0) == 1) {
-                    if (down && repeatCount == 0) {
-                        mHandler.postDelayed(mBackLongPress, mBackKillTimeout);
-                    }
-                }
                 if (!mRecentAppsPreloaded && (mPressOnBackBehavior.equals(getStr(KEY_ACTION_APP_SWITCH)) ||
                         mLongPressOnBackBehavior.equals(getStr(KEY_ACTION_APP_SWITCH)))) {
                     preloadRecentApps();
@@ -5582,4 +5576,3 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 pw.print(" mHdmiRotationLock="); pw.println(mHdmiRotationLock);
     }
 }
-
