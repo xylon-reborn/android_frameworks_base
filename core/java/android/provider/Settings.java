a299c9863cb2df6112458cc1c5d649d3abbabca3
itsmikeramsay <ronaldramsayii@gmail.com>
2013-03-25 12:57:07 -0400

    Revert "Volume Button Swap" Already done: https://github.com/DeviantDevs/android_packages_apps_Settings/commit/25f6c39af13fdcdc5913112807394ae84fad411f
    See Settings > Display > Rotation
    
    Nice job tho bro!
    
    This reverts commit 0e2c48a34ad3e311122e20dc71978dd2a22c2771.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2603,15 +2603,6 @@ public final class Settings {
          * @hide
          */
         public static final String KEY_ASSIST_ACTION = "key_assist_action";
-		
-		/**
-         * Swap volume buttons when the screen is rotated
-         * 0 - Disabled
-         * 1 - Enabled (screen is rotated by 90 or 180 degrees: phone, hybrid)
-         * 2 - Enabled (screen is rotated by 180 or 270 degrees: tablet)
-         * @hide
-         */
-         public static final String SWAP_VOLUME_KEYS_ON_ROTATION = "swap_volume_keys_on_rotation";
 
         /**
          * Action to perform when the assistant (search) key is long-pressed. (Default is 4)
