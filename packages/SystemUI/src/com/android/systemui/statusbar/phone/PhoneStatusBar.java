f48b244484e01bd78c9d2957bdb6df0bdee179cd
Lars Greiss <kufikugel@googlemail.com>
2013-03-29 02:52:06 +0100

    Frameworks: Fix recent show from navbar sometimes weird
    things
    
    cheers
    
    Change-Id: Ib7069be558a3e3ff0c058bb636a09726760863f6

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -979,13 +979,6 @@ public class PhoneStatusBar extends BaseStatusBar {
         return mNaturalBarHeight;
     }
 
-    private final View.OnClickListener mRecentsClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            toggleRecentApps();
-        }
-    };
-
     private int mShowSearchHoldoff = 0;
     private final Runnable mShowSearchPanel = new Runnable() {
         @Override
