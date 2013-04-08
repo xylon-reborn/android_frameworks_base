16f5cb4993ab50abadaf60504e903ca8e7923d60
gs2usr <jglarios1@gmail.com>
2013-04-08 15:13:34 -0700

    FIx: PhabletUI NavBar in landscape
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/NavigationBarView.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -872,17 +872,17 @@ public class NavigationBarView extends LinearLayout implements BaseStatusBar.Nav
     }
 
     public void reorient() {
-        final int rot = mDisplay.getRotation();
-        for (int i=0; i<4; i++) {
+        int rot = mDisplay.getRotation();
+        for (int i=0; i<3; i++) {
             mRotatedViews[i].setVisibility(View.GONE);
         }
-        if (mCurrentUIMode !=0) { // this is either a tablet of Phablet.  Need to stay at Rot_0
+        if (mSystemUiLayout != 360) { // this is either a tablet of Phablet.
             mCurrentView = mRotatedViews[Surface.ROTATION_0];
         } else {
             mCurrentView = mRotatedViews[rot];
         }
-        mCurrentView.setVisibility(View.VISIBLE);
 
+        mCurrentView.setVisibility(View.VISIBLE);
         mDeadZone = (DeadZone) mCurrentView.findViewById(R.id.deadzone);
 
         // force the low profile & disabled states into compliance
