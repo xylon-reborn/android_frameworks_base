SlimRoms/frameworks_base

Frameworks: Add Widgets viewpager for all devices (2/2)
First thanks AOKP for the basic idea and some parts of code.
This commit gives the ability to show up a viewpager with
custom user set widgets. It is bindable to navbar, navring
and to hardwarekeyboards. It works with navbar enabled as
well as with navbar disabled. Like every time we polished it
Slimusual a bit up.

Change to the original widgets viewpager
- bindable also over hardwarebuttons
- shows also up without navbar
- color settings for background and text
- transparency of background (default 25%)
- settings complete overwork
- TRDS compatible
- complete overwork of widget height handling
- multiDpi (thanks mnazim)
- slimup and a lot improvements and fixes

cheers

Change-Id: I64c1a56131616769588500c0faedb7eec6f59249

packages/SystemUI/src/com/android/systemui/slim/WidgetToggle.java
@@ -1,0 +1,48 @@
+/*
+ * Copyright 2011 AOKP by Mike Wilson - Zaphod-Beeblebrox
+ * Copyright 2013 SlimRoms
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.slim;
+
+import com.android.systemui.statusbar.WidgetView;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/*
+ * Toggle the Widget
+ */
+
+public class WidgetToggle extends Activity  {
+  public WidgetToggle() {
+    super();
+  }
+
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    Intent toggleWidgets = new Intent(
+            WidgetView.WidgetReceiver.ACTION_TOGGLE_WIDGETS);
+    sendBroadcast(toggleWidgets);
+    this.finish();
+  }
+}

xylon-reborn/android_frameworks_base

Conflicts:
	core/java/android/provider/Settings.java
	packages/SystemUI/res/drawable-hdpi/ic_sysbar_widget.png
	packages/SystemUI/res/drawable-mdpi/ic_sysbar_widget.png
	packages/SystemUI/res/drawable-xhdpi/ic_sysbar_widget.png
	packages/SystemUI/res/layout/navigation_bar_expanded.xml
	packages/SystemUI/src/com/android/systemui/WidgetSelectActivity.java
	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
	packages/SystemUI/src/com/android/systemui/statusbar/WidgetPagerAdapter.java
	packages/SystemUI/src/com/android/systemui/statusbar/WidgetView.java

packages/SystemUI/src/com/android/systemui/liquid/WidgetToggle.java
@@ -1,0 +1,48 @@
+/*
+ * Copyright 2011 AOKP by Mike Wilson - Zaphod-Beeblebrox
+ * Copyright 2013 SlimRoms
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.liquid;
+
+import com.android.systemui.statusbar.WidgetView;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/*
+ * Toggle the Widget
+ */
+
+public class WidgetToggle extends Activity  {
+  public WidgetToggle() {
+    super();
+  }
+
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    Intent toggleWidgets = new Intent(
+            WidgetView.WidgetReceiver.ACTION_TOGGLE_WIDGETS);
+    sendBroadcast(toggleWidgets);
+    this.finish();
+  }
+}