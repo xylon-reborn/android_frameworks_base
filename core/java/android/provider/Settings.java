751dc782651a1dc13929d4d1f106660c7749ef83
liquid0624 <rjbirnesser@gmail.com>
2013-04-18 21:51:46 -0400

    Revert "PIE: compressed layout, stubs, new defaults"
    This reverts commit 31c34e57a870b53ba691d8a6bf6bf67bc8ef5291.
    
    Conflicts:
    	packages/SystemUI/res/values/dimens.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3151,7 +3151,7 @@ public final class Settings {
         public static final String PIE_CONTROLS = "pie_controls";
 
         /**
-         * Pie menu, should default to 1 (yes, show)
+         * Pie menu, should default to 0 (no, show only when needed)
          * @hide
          */
         public static final String PIE_MENU = "pie_menu";
@@ -3163,7 +3163,7 @@ public final class Settings {
         public static final String PIE_SEARCH = "pie_search";
 
         /**
-         * Pie will not rotate. Should default to 1 (yes, do not rotate)
+         * Pie will not rotate. Should default to 0 (no, rotate)
          * @hide
          */
         public static final String PIE_STICK = "pie_stick";
@@ -3175,7 +3175,7 @@ public final class Settings {
         public static final String PIE_LAST_APP = "pie_last_app";
 
         /**
-         * Pie gap angle, should default to 3
+         * Pie gap angle, should default to 1
          * @hide
          */
         public static final String PIE_GAP = "pie_gap";
