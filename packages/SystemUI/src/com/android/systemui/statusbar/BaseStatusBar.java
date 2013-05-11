830034f694244a587e21710be44f0c305cc129d2
Muhammed Nazim <mnm9994u@gmail.com>
2013-05-11 11:19:28 +0200

    Add Statusbar Quick Peek [1/2]
    Adds the ability to peek at the statusbar when hidden
    by swiping down from the top edge of the screen.
    
    PS 1/3 : Fixed Derp!
    PS 4   : Added Fullscreen app support!
    PS 5   : Added Pie Detection
    PS 6   : Derp forgot the TOP Flag!
    PS 7/9 : Some fixes
    
    Change-Id: I4beda91e21518cd2fa391ff1b0fe23ea8e24370e
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -36,6 +36,7 @@ import com.android.systemui.statusbar.policy.PieController.Position;
 import com.android.systemui.statusbar.tablet.StatusBarPanel;
 import com.android.systemui.statusbar.WidgetView;
 
+import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.ActivityManagerNative;
 import android.app.ActivityOptions;
@@ -1858,6 +1859,8 @@ public abstract class BaseStatusBar extends SystemUI implements
                 refreshPieTriggers();
             }
         }
+
+        Activity.mPieOnTop = isPieEnabled() && ((Position.TOP.FLAG & mPieTriggerSlots & mPieTriggerMask) != 0);
     }
 
     // This should only be called, when is is clear that the pie controls are active
