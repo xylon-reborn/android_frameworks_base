05580c034dd536516fc5a509af7ccc11e757a1a1
Muhammed Nazim <mnm9994u@gmail.com>
2013-05-19 11:47:48 +0200

    Fix Logical error
    Change-Id: I0b07ea92cd9b747772bf1c2376a6a801d524b7d0

--- media/java/android/media/AudioService.java
+++ media/java/android/media/AudioService.java
@@ -772,7 +772,7 @@ public class AudioService extends IAudioService.Stub implements OnFinished {
             mSafeVolumeEnabled = new Boolean(safeVolumeEnabled(cr));
 
             mVolumeKeysControlRingStream = Settings.System.getIntForUser(cr,
-                    Settings.System.VOLUME_KEYS_CONTROL_RING_STREAM, 1, UserHandle.USER_CURRENT) == 1;
+                    Settings.System.VOLUME_KEYS_CONTROL_RING_STREAM, 0, UserHandle.USER_CURRENT) == 0;
         }
 
         mLinkNotificationWithVolume = Settings.System.getIntForUser(cr,
@@ -3610,7 +3610,7 @@ public class AudioService extends IAudioService.Stub implements OnFinished {
                     mSafeVolumeEnabled = safeVolumeEnabled(mContentResolver);
                 } else if (uri.equals(Settings.System.getUriFor(Settings.System.VOLUME_KEYS_CONTROL_RING_STREAM))) {
                     mVolumeKeysControlRingStream = Settings.System.getIntForUser(mContentResolver,
-                            Settings.System.VOLUME_KEYS_CONTROL_RING_STREAM, 1, UserHandle.USER_CURRENT) == 1;
+                            Settings.System.VOLUME_KEYS_CONTROL_RING_STREAM, 0, UserHandle.USER_CURRENT) == 0;
                 }
             }
         }
