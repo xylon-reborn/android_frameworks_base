SlimRoms/frameworks_base

Frameworks: bring navring/pie/navbar vibrate/sound inline with AOSP
currently all 3 features are doing it a bit different. For navigation
AOSP has for normal click:

 short soft vibrate + click sound

for longpress

 no sound but more intensive vibrate

This commit does:
- seperate keyevents haptic and sound from the other actions and call them
- add vibrate and sound to navring
- navbar only vibrate when the action is performing
- do not use custom vibrate on keycodes
- bring all inline with AOSP like described above
- fix a possible NPE on lockcreen

not solveable (do not ask me...google is not always consistent especially
on keyevents) due the change will break other global behaviours:
- menu button always have a click sound even on longpress.
- powermenu always have a second vibrate due of GlobalManager

all other works inline like described

Change-Id: I90fae2393c5c654055014556b0772447313d28b4

packages/SystemUI/src/com/android/systemui/SearchPanelView.java
@@ -50,8 +50,10 @@ import android.util.EventLog;
 import android.util.Slog;
 import android.util.Log;
 import android.util.TypedValue;
+import android.view.HapticFeedbackConstants;
 import android.view.IWindowManager;
 import android.view.MotionEvent;
+import android.view.SoundEffectConstants;
 import android.view.View;
 import android.view.ViewConfiguration;
 import android.view.ViewGroup;
@@ -146,8 +148,11 @@ public class SearchPanelView extends FrameLayout implements
                 if (!mSearchPanelLock) {
                     mLongPress = true;
                     mBar.hideSearchPanel();
+                    if (!SlimActions.isActionKeyEvent(longList.get(mTarget))) {
+                        performHapticFeedback(HapticFeedbackConstants.LONG_PRESS);
+                    }
                     sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_LONG_CLICKED);
-                    SlimActions.processAction(mContext, longList.get(mTarget));
+                    SlimActions.processAction(mContext, longList.get(mTarget), true);
                     mSearchPanelLock = true;
                  }
             }
@@ -186,8 +191,14 @@ public class SearchPanelView extends FrameLayout implements
             final int resId = mGlowPadView.getResourceIdForTarget(target);
             mTarget = target;
             if (!mLongPress) {
+                if (!SlimActions.isActionKeyEvent(intentList.get(target))) {
+                    performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
+                }
+                if (!intentList.get(target).equals(ButtonsConstants.ACTION_MENU)) {
+                    playSoundEffect(SoundEffectConstants.CLICK);
+                }
                 sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_CLICKED);
-                SlimActions.processAction(mContext, intentList.get(target));
+                SlimActions.processAction(mContext, intentList.get(target), false);
                 mHandler.removeCallbacks(SetLongPress);
             }
         }

packages/SystemUI/src/com/android/systemui/statusbar/policy/KeyButtonView.java
@@ -61,8 +61,8 @@ public class KeyButtonView extends ImageView {
 
     long mDownTime;
     int mCode;
-    String mClickAction;
-    String mLongpressAction;
+    String mClickAction = ButtonsConstants.ACTION_NULL;
+    String mLongpressAction = ButtonsConstants.ACTION_NULL;
     int mTouchSlop;
     Drawable mGlowBG;
     static int mGlowBGColor = Integer.MIN_VALUE;
@@ -82,7 +82,11 @@ public class KeyButtonView extends ImageView {
             mIsLongpressed = true;
             if (isPressed()) {
                 sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_LONG_CLICKED);
+                if (SlimActions.isActionKeyEvent(mLongpressAction)) {
+                    setHapticFeedbackEnabled(false);
+                }
                 performLongClick();
+                setHapticFeedbackEnabled(true);
             }
         }
     };
@@ -151,8 +155,8 @@ public class KeyButtonView extends ImageView {
     }
 
     public void setLongpressAction(String action) {
+        mLongpressAction = action;
         if (!action.equals(ButtonsConstants.ACTION_NULL)) {
-            mLongpressAction = action;
             mSupportsLongpress = true;
             setOnLongClickListener(mLongPressListener);
         }
@@ -298,9 +302,6 @@ public class KeyButtonView extends ImageView {
                 setPressed(true);
                 if (mCode != 0) {
                     sendEvent(KeyEvent.ACTION_DOWN, 0, mDownTime);
-                } else if (!SlimActions.isActionKeyEvent(mClickAction)) {
-                    // Provide the same haptic feedback that the system offers for virtual keys.
-                    performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
                 }
                 if (mSupportsLongpress) {
                     removeCallbacks(mCheckLongPress);
@@ -337,6 +338,9 @@ public class KeyButtonView extends ImageView {
                     } else {
                         // no key code, it is a custom click action
                         if (doIt) {
+                            if (!SlimActions.isActionKeyEvent(mClickAction)) {
+                                performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
+                            }
                             performClick();
                         }
                     }
@@ -356,7 +360,7 @@ public class KeyButtonView extends ImageView {
     private OnClickListener mClickListener = new OnClickListener() {
         @Override
         public void onClick(View v) {
-            SlimActions.processAction(mContext, mClickAction);
+            SlimActions.processAction(mContext, mClickAction, false);
             return;
         }
     };
@@ -364,7 +368,7 @@ public class KeyButtonView extends ImageView {
     private OnLongClickListener mLongPressListener = new OnLongClickListener() {
         @Override
         public boolean onLongClick(View v) {
-            SlimActions.processAction(mContext, mLongpressAction);
+            SlimActions.processAction(mContext, mLongpressAction, true);
             return true;
         }
     };

packages/SystemUI/src/com/android/systemui/statusbar/policy/PieController.java
@@ -745,11 +745,8 @@ public class PieController implements BaseStatusBar.NavigationBarCallback,
         if (!SlimActions.isActionKeyEvent(type)) {
             mPieContainer.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS);
         }
-        if (!type.equals(ButtonsConstants.ACTION_MENU)) {
-            mPieContainer.playSoundEffect(SoundEffectConstants.CLICK);
-        }
         mPieContainer.sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_LONG_CLICKED);
-        SlimActions.processAction(mContext, type);
+        SlimActions.processAction(mContext, type, true);
     }
 
     @Override
@@ -762,7 +759,7 @@ public class PieController implements BaseStatusBar.NavigationBarCallback,
             mPieContainer.playSoundEffect(SoundEffectConstants.CLICK);
         }
         mPieContainer.sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_CLICKED);
-        SlimActions.processAction(mContext, type);
+        SlimActions.processAction(mContext, type, false);
     }
 
     private void doHapticTriggerFeedback() {

xylon-reborn/android_frameworks_base

Conflicts:
	core/java/com/android/internal/util/liquid/LiquidActions.java
	packages/SystemUI/src/com/android/systemui/SearchPanelView.java
	packages/SystemUI/src/com/android/systemui/statusbar/policy/KeyButtonView.java
	packages/SystemUI/src/com/android/systemui/statusbar/policy/PieController.java

packages/SystemUI/src/com/android/systemui/SearchPanelView.java
@@ -50,8 +50,10 @@ import android.util.EventLog;
 import android.util.Slog;
 import android.util.Log;
 import android.util.TypedValue;
+import android.view.HapticFeedbackConstants;
 import android.view.IWindowManager;
 import android.view.MotionEvent;
+import android.view.SoundEffectConstants;
 import android.view.View;
 import android.view.ViewConfiguration;
 import android.view.ViewGroup;
@@ -146,8 +148,11 @@ public class SearchPanelView extends FrameLayout implements
                 if (!mSearchPanelLock) {
                     mLongPress = true;
                     mBar.hideSearchPanel();
+                    if (!LiquidActions.isActionKeyEvent(longList.get(mTarget))) {
+                        performHapticFeedback(HapticFeedbackConstants.LONG_PRESS);
+                    }
                     sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_LONG_CLICKED);
-                    LiquidActions.processAction(mContext, longList.get(mTarget));
+                    LiquidActions.processAction(mContext, longList.get(mTarget), true);
                     mSearchPanelLock = true;
                  }
             }
@@ -186,8 +191,14 @@ public class SearchPanelView extends FrameLayout implements
             final int resId = mGlowPadView.getResourceIdForTarget(target);
             mTarget = target;
             if (!mLongPress) {
+                if (!LiquidActions.isActionKeyEvent(intentList.get(target))) {
+                    performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
+                }
+                if (!intentList.get(target).equals(ButtonsConstants.ACTION_MENU)) {
+                    playSoundEffect(SoundEffectConstants.CLICK);
+                }
                 sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_CLICKED);
-                LiquidActions.processAction(mContext, intentList.get(target));
+                LiquidActions.processAction(mContext, intentList.get(target), false);
                 mHandler.removeCallbacks(SetLongPress);
             }
         }

packages/SystemUI/src/com/android/systemui/statusbar/policy/KeyButtonView.java
@@ -61,8 +61,8 @@ public class KeyButtonView extends ImageView {
 
     long mDownTime;
     int mCode;
-    String mClickAction;
-    String mLongpressAction;
+    String mClickAction = ButtonsConstants.ACTION_NULL;
+    String mLongpressAction = ButtonsConstants.ACTION_NULL;
     int mTouchSlop;
     Drawable mGlowBG;
     static int mGlowBGColor = Integer.MIN_VALUE;
@@ -82,7 +82,11 @@ public class KeyButtonView extends ImageView {
             mIsLongpressed = true;
             if (isPressed()) {
                 sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_LONG_CLICKED);
+                if (LiquidActions.isActionKeyEvent(mLongpressAction)) {
+                    setHapticFeedbackEnabled(false);
+                }
                 performLongClick();
+                setHapticFeedbackEnabled(true);
             }
         }
     };
@@ -151,8 +155,8 @@ public class KeyButtonView extends ImageView {
     }
 
     public void setLongpressAction(String action) {
+        mLongpressAction = action;
         if (!action.equals(ButtonsConstants.ACTION_NULL)) {
-            mLongpressAction = action;
             mSupportsLongpress = true;
             setOnLongClickListener(mLongPressListener);
         }
@@ -298,9 +302,6 @@ public class KeyButtonView extends ImageView {
                 setPressed(true);
                 if (mCode != 0) {
                     sendEvent(KeyEvent.ACTION_DOWN, 0, mDownTime);
-                } else if (!LiquidActions.isActionKeyEvent(mClickAction)) {
-                    // Provide the same haptic feedback that the system offers for virtual keys.
-                    performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
                 }
                 if (mSupportsLongpress) {
                     removeCallbacks(mCheckLongPress);
@@ -337,6 +338,9 @@ public class KeyButtonView extends ImageView {
                     } else {
                         // no key code, it is a custom click action
                         if (doIt) {
+                            if (!LiquidActions.isActionKeyEvent(mClickAction)) {
+                                performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
+                            }
                             performClick();
                         }
                     }
@@ -356,7 +360,7 @@ public class KeyButtonView extends ImageView {
     private OnClickListener mClickListener = new OnClickListener() {
         @Override
         public void onClick(View v) {
-            LiquidActions.processAction(mContext, mClickAction);
+            LiquidActions.processAction(mContext, mClickAction, false);
             return;
         }
     };
@@ -364,7 +368,7 @@ public class KeyButtonView extends ImageView {
     private OnLongClickListener mLongPressListener = new OnLongClickListener() {
         @Override
         public boolean onLongClick(View v) {
-            LiquidActions.processAction(mContext, mLongpressAction);
+            LiquidActions.processAction(mContext, mLongpressAction, true);
             return true;
         }
     };

packages/SystemUI/src/com/android/systemui/statusbar/policy/PieController.java
@@ -745,11 +745,8 @@ public class PieController implements BaseStatusBar.NavigationBarCallback,
         if (!LiquidActions.isActionKeyEvent(type)) {
             mPieContainer.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS);
         }
-        if (!type.equals(ButtonsConstants.ACTION_MENU)) {
-            mPieContainer.playSoundEffect(SoundEffectConstants.CLICK);
-        }
         mPieContainer.sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_LONG_CLICKED);
-        LiquidActions.processAction(mContext, type);
+        LiquidActions.processAction(mContext, type, true);
     }
 
     @Override
@@ -763,7 +760,7 @@ public class PieController implements BaseStatusBar.NavigationBarCallback,
             mPieContainer.playSoundEffect(SoundEffectConstants.CLICK);
         }
         mPieContainer.sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_CLICKED);
-        LiquidActions.processAction(mContext, type);
+        LiquidActions.processAction(mContext, type, false);
     }
 
     private void doHapticTriggerFeedback() {