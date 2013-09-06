79994566a9d8237252c44fe9834edf5550d85322
Danesh M <daneshm90@gmail.com>
2013-09-05 19:23:15 -0700

    Privacy Guard : Reimplement backed by AppOps
    Re-implements privacy guard to use appops.
    
    Change-Id: I2b52e8cf0d6e126fb156f83df885b5551503cd9d
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/res/res/drawable-hdpi/stat_notify_privacy_guard.png
    	core/res/res/drawable-mdpi/stat_notify_privacy_guard.png
    	core/res/res/drawable-xhdpi/stat_notify_privacy_guard.png
    	core/res/res/drawable-xxhdpi/stat_notify_privacy_guard.png
    	core/res/res/values/cm_strings.xml
    	core/res/res/values/symbols.xml
    	services/java/com/android/server/am/ActivityStack.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6242,6 +6242,12 @@ public final class Settings {
         public static final String DIALPAD_AUTOCOMPLETE = "dialpad_autocomplete";
 
         /**
+         * Whether newly installed apps should run with privacy guard by default
+         * @hide
+         */
+         public static final String PRIVACY_GUARD_DEFAULT = "privacy_guard_default";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
@@ -6288,7 +6294,8 @@ public final class Settings {
             UI_INVERTED_MODE,
             LOCK_SCREEN_OWNER_INFO,
             LOCK_SCREEN_OWNER_INFO_ENABLED,
-            DIALPAD_AUTOCOMPLETE
+            DIALPAD_AUTOCOMPLETE,
+            PRIVACY_GUARD_DEFAULT
         };
 
         /**
