10c47e945e6a5387cb0e7e0a86cba233e505f212
liquid0624 <rjbirnesser@gmail.com>
2013-09-19 18:17:55 -0400

    Change halo update method to protected

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -591,7 +591,7 @@ public abstract class BaseStatusBar extends SystemUI implements
         updateHalo();
     }
 
-    public void updateHalo() {
+    protected void updateHalo() {
         mHaloEnabled = Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.HALO_ENABLED, 0) == 1;
         mHaloActive = Settings.System.getInt(mContext.getContentResolver(),
