b14f408bc941ad21e670707e47fdc0f8407f2181
Alex <mazdarider23@gmail.com>
2013-10-06 06:14:59 -0500

    Removed old Traffic indiciator and added MIUI-like one from ChaOS
    Credit and Thanks to 0xD34D/ChaOS team.
    
    Change-Id: I11cabc3001aaf4129b60d361b7b37dce104e0f04
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/layout/status_bar.xml
    	packages/SystemUI/res/values/dimens.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4395,6 +4395,18 @@ public final class Settings {
         public static final String STATUS_BAR_TRAFFIC = "status_bar_traffic";
 
         /**
+         * Whether to show the network status in the status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_NETWORK_STATS = "status_bar_network_stats";
+
+        /**
+         * Frequency at which stats are updated, in milliseconds
+         * @hide
+         */
+        public static final String STATUS_BAR_NETWORK_STATS_UPDATE_INTERVAL = "status_bar_network_stats_update_frequency";
+
+        /**
          * Status icon color.
          *
          * @hide
